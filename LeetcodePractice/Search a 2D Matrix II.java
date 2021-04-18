fa4696a0b7f0730886322161334427b0497fdadc
Yikun Li <lykunkka1995@gmail.com>
2021-04-17 19:43:48 -0500

    Create Search a 2D Matrix II.java

---
+++ LeetcodePractice/Search a 2D Matrix II.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // corner case
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        // process
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
