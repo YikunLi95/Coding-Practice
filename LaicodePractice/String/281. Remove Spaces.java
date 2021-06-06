a1ae355da792d71abdd9874e3c48f25056c922cc
Yikun Li <lykunkka1995@gmail.com>
2021-06-05 19:47:31 -0500

    Create 281. Remove Spaces.java

---
+++ LaicodePractice/String/281. Remove Spaces.java
@@ -0,0 +1,32 @@
+/*
+Given a string, remove all leading/trailing/duplicated empty spaces.
+
+Assumptions:
+The given string is not null.
+
+Examples:
+“  a” --> “a”
+“   I     love MTV ” --> “I love MTV”
+*/
+
+public class Solution {
+  public String removeSpaces(String input) {
+    // Write your solution here
+    if (input.isEmpty()) {
+      return input;
+    }
+    char[] array = input.toCharArray();
+    int end = 0;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == ' ' && (i == 0 || array[i - 1] == ' ')) {
+        continue;
+      }
+      array[end] = array[i];
+      end++;
+    }
+    if (end > 0 && array[end - 1] == ' ') {
+      end--;
+    }
+    return new String(array, 0, end);
+  }
+}
