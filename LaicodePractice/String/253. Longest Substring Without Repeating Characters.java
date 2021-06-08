8ac6800987f6633e02700a9f3abc7819c900ae81
Yikun Li <lykunkka1995@gmail.com>
2021-06-08 16:22:16 -0500

    Create 253. Longest Substring Without Repeating Characters.java

---
+++ LaicodePractice/String/253. Longest Substring Without Repeating Characters.java
@@ -0,0 +1,24 @@
+/*
+Given a string, find the longest substring without any repeating characters and return the length of it. The input string is guaranteed to be not null.
+
+For example, the longest substring without repeating letters for "bcdfbd" is "bcdf", we should return 4 in this case.
+*/
+
+public class Solution {
+  public int longest(String input) {
+    // Write your solution here
+    Set<Character> distinct = new HashSet<>();
+    int slow = 0;
+    int fast = 0;
+    int longest = 0;
+    while (fast < input.length()) {
+      if (distinct.contains(input.charAt(fast))) {
+        distinct.remove(input.charAt(slow++));
+      } else {
+        distinct.add(input.charAt(fast++));
+        longest = Math.max(longest, fast - slow);
+      }
+    }
+    return longest;
+  }
+}
