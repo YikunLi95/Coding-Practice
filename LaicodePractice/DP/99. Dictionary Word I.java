0767853e9f182636d4ff4e1df3ea1be44fe803f9
Yikun Li <lykunkka1995@gmail.com>
2021-06-18 23:47:00 -0500

    Update 99. Dictionary Word I.java

--- LaicodePractice/DP/99. Dictionary Word I.java
+++ LaicodePractice/DP/99. Dictionary Word I.java
@@ -18,18 +18,21 @@ Word: “robcatbob” return true since it can be composed by "rob", "cat", "bob
 public class Solution {
   public boolean canBreak(String input, String[] dict) {
     // Write your solution here
-    Set<String> dictSet = toSet(dict);
-    boolean[] canBreak = new boolean[input.length() + 1];
-    canBreak[0] = true;
-    for (int i = 1; i <= input.length(); i++) {
-      for (int j = 0; j < i; j++) {
-        if (canBreak[j] && dictSet.contains(input.substring(j, i))) {
-          canBreak[i] = true;
+    Set<String> set = toSet(dict);
+    // M[i] represents the substring of input [0, i), whether is valid
+    // M[0] represents "", M[1] represents input[0], M[2] represents input[0, 1]
+    boolean[] M = new boolean[input.length() + 1];
+
+    M[0] = true;
+    for (int i = 1; i <= input.length(); i++) { // i represents # of letters
+      for (int j = 0; j < i; j++) { // j represents the cutting position
+        if (M[j] && set.contains(input.substring(j, i))) { // substring(i, j) represents the [i, j - 1] indices
+          M[i] = true;
           break;
         }
       }
     }
-    return canBreak[input.length()];
+    return M[input.length()];
   }
 
   private Set<String> toSet(String[] dict) {
@@ -38,5 +41,6 @@ public class Solution {
       set.add(s);
     }
     return set;
-  } // TC: O(n ^ 3) SC: O(n)
+  }
 }
+// TC: O(n ^ 3) SC: O(n) -> n represents the length of input string
