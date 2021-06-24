499921be010b2cd8adfd96ce9382478dc3d25c11
Yikun Li <lykunkka1995@gmail.com>
2021-06-24 17:39:26 -0500

    Create 68. Missing Number I.java

---
+++ LaicodePractice/HashTable/68. Missing Number I.java
@@ -0,0 +1,29 @@
+/*
+Given an integer array of size N - 1, containing all the numbers from 1 to N except one, find the missing number.
+
+Assumptions
+
+The given array is not null, and N >= 1
+Examples
+
+A = {2, 1, 4}, the missing number is 3
+A = {1, 2, 3}, the missing number is 4
+A = {}, the missing number is 1
+*/
+
+public class Solution {
+  public int missing(int[] array) {
+    // Write your solution here
+    int n = array.length + 1;
+    HashSet<Integer> set = new HashSet<Integer>();
+    for (int num : array) {
+      set.add(num);
+    }
+    for (int i = 1; i < n; i++) {
+      if (!set.contains(i)) {
+        return i;
+      }
+    }
+    return n;
+  }
+}
