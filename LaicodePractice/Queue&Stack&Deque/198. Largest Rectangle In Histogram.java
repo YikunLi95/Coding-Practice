b2687b30ca72a6aa7a38e6710e3332112b70e379
Yikun Li <lykunkka1995@gmail.com>
2021-06-30 18:47:20 -0500

    Create 198. Largest Rectangle In Histogram.java

---
+++ LaicodePractice/Queue&Stack&Deque/198. Largest Rectangle In Histogram.java
@@ -0,0 +1,28 @@
+/*
+Given a non-negative integer array representing the heights of a list of adjacent bars. Suppose each bar has a width of 1. Find the largest rectangular area that can be formed in the histogram.
+
+Assumptions
+
+The given array is not null or empty
+Examples
+
+{ 2, 1, 3, 3, 4 }, the largest rectangle area is 3 * 3 = 9(starting from index 2 and ending at index 4)
+*/
+
+public class Solution {
+  public int largest(int[] array) {
+    // Write your solution here
+    int res = 0;
+    Deque<Integer> stack = new ArrayDeque<>();
+    for (int i = 0; i <= array.length; i++) {
+      int cur = i == array.length ? 0 : array[i];
+      while (!stack.isEmpty() && array[stack.peekFirst()] >= cur) {
+        int height = array[stack.pollFirst()];
+        int left = stack.isEmpty() ? 0 : stack.peekFirst() + 1;
+        res = Math.max(height * (i - left), res);
+      }
+      stack.offerFirst(i);
+    }
+    return res;
+  }
+}
