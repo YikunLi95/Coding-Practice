20cdcf31c79698eb4a042c79871c95c63482d980
Yikun Li <lykunkka1995@gmail.com>
2021-07-01 19:13:42 -0500

    Create 108. Perfect Shuffle.java

---
+++ LaicodePractice/Probability, Sampling & Randomization/108. Perfect Shuffle.java
@@ -0,0 +1,26 @@
+/*
+Given an array of integers (without any duplicates), shuffle the array such that all permutations are equally likely to be generated.
+
+Assumptions
+
+The given array is not null
+*/
+
+public class Solution {
+  public void shuffle(int[] array) {
+    // Write your solution here.
+    if (array.length <= 1) {
+      return;
+    }
+    for (int i = array.length; i >= 1; i--) {
+      int idx = (int) (Math.random() * i);
+      swap(array, i - 1, idx);
+    }
+  }
+
+  private void swap(int[] array, int x, int y) {
+    int tmp = array[x];
+    array[x] = array[y];
+    array[y] = tmp;
+  }
+}
