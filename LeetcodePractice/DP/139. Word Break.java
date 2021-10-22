57ff77d76653e46cb47b281eb2b83cf636ec8eca
Yikun Li <yikunli10@gmail.com>
2021-10-21 21:27:31 -0500

    Create 139. Word Break.java

---
+++ LeetcodePractice/DP/139. Word Break.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> dictSet = toSet(wordDict);
+        boolean[] ans = new boolean[s.length() + 1];
+        ans[0] = true;
+        
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (ans[j] && dictSet.contains(s.substring(j, i))) {
+                    ans[i] = true;
+                    break;
+                }
+            }
+        }
+        return ans[s.length()];
+    }
+    
+    private Set<String> toSet(List<String> wordDict) {
+        Set<String> dictSet = new HashSet<>();
+        for (String s : wordDict) {
+            dictSet.add(s);
+        }
+        return dictSet;
+    }
+}
+// TC: O(n^2)
+// SC: O(n)
